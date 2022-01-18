2e3a5fc2bacc9996e50e4900d699a8fb995cd14a
Mathias Nikkel <mathiasnikkel@gmail.com>
2022-01-18 16:07:09 -0300

    reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -8,4 +8,14 @@ class PasswordResetsController < ApplicationController
 
     redirect_to root_path, notice: 'check your email'
   end
+
+  def edit
+    @user = User.find_signed!(params[:token], purpose: "password_reset")
+  rescue ActiveSupport::MessageVerifier::InvalidSignature
+    redirect_to sign_in_path, alert: "token expired"
+  end
+
+  def update
+    redirect_to root_path, alert: "Password reseted"
+  end
 end
